e4259b5c61af6ddf235ca7b4705df3b70952bea2
git-pusher <rachel.palomeque@gmail.com>
2019-07-11 01:53:38 -0500

    BACK

---
+++ models/Registro.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+const Schema = mongoose.Schema;
+
+const registroSchema = new Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    nombreUsuario: {
+        unique: true,
+        type: String,
+        required: true
+    },  
+    //ESTE ES EL HASH GENERADO CON EL PASSWORD Y EL SALT  
+    password: {
+        type: String,
+        required: true
+    },
+    //ESTE ES EL SALT
+    hash: {
+        type: String
+    },
+    estado:{
+        type: String,
+        default: "A"
+    }
+    }, {timestamps: true});
+
+
+const Registro = mongoose.model('Registro', registroSchema);
+module.exports = {Registro};
